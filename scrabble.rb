96a784d21451a42c4c7c3b13f43cb0e285d843e7
Alysia Brown <alysiacbrown@gmail.com>
2016-04-07 15:14:33 -0700

    Finished the /score page.

--- scrabble.rb
+++ scrabble.rb
@@ -1,25 +1,34 @@
 require 'sinatra'
+require_relative 'lib/score'
+require_relative 'lib/score-many'
 
-class Scrabble < Sinatra::Base
+class MyScoring < Sinatra::Base
 
   get '/' do
+    @page_title = "Scrabble Scoring"
     erb :index
   end
 
   get '/score' do
+    @page_title = "Score A Word"
     erb :score
   end
 
   get '/score-many' do
+    @page_title = "Score Many Words"
     erb :score_many
   end
 
   post '/score' do
-    # ;ladkjfalsdkfj
+    @page_title = "Score A Word"
+    @word = Scrabble::Word.new(params["word_hash"]["word"])
+    erb :score
   end
 
-  post 'score_many' do
-    # alskdfjlsdkfj
+  post '/score_many' do
+  @page_title = "Score Many Words"
+  @word = Scrabble::Word.new(params["word_hash"]["word"])
+  erb :score-many
   end
 
   run!
